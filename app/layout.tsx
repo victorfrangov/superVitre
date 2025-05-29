159d0a231669cb80c3fc10737a02043e562d5c4d
Victor Frangov <59508395+vict0rfr@users.noreply.github.com>
2025-05-29 12:55:44 -0400

    fixed stupid error

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V3 && (
           <Script
-            src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V3}`}
+            src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_SITE_CAPTCHA_KEY}`}
             strategy="beforeInteractive"
           />
-        )}
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
